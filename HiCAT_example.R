7e5be4c2e66b98530a96517e3d6a7ebe351a0e33
combio-dku <82195405+combio-dku@users.noreply.github.com>
2022-07-27 22:14:17 +0900

    Add files via upload

---
+++ HiCAT_example.R
@@ -0,0 +1,26 @@
+suppressPackageStartupMessages(library(anndata))
+suppressPackageStartupMessages(library(reticulate))
+
+### Load MarkerCount
+mkrcnt <- import("MarkerCount.hicat")
+
+### Load data & extract cell-gene matrix as a data.frame (rownames: cell barcode, colnames: gene symbol)
+adata_test <- read_h5ad('Melanoma_5K_rev.h5ad')
+X_test <- adata_test$to_df()
+celltype_major <- as.character(adata_test$obs[,'celltype_major'])
+celltype_minor <- as.character(adata_test$obs[,'celltype_minor'])
+
+### Run MarkerCOunt_GSE
+mkr_file <- 'cell_markers_rndsystems_rev.tsv'
+
+lst_res <- mkrcnt$HiCAT( X_test, marker_file = mkr_file, log_transformed = FALSE ) #, verbose = FALSE )
+
+df_pred <- lst_res[[1]]
+summary <- lst_res[[2]]
+
+### Check result: First 3 columns are major cell type, minor cell type, cell type subset, respectively
+### Compare them with the pre-annotated cell type
+
+df_pred[,'celltype_major'] <- celltype_major
+df_pred[,'celltype_minor'] <- celltype_minor
+head(df_pred)
